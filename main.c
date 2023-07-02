8f2018bec9a1111b280e7f891fce5951a8e0ff91
Lucas Ferreira <lukasnoobre53@gmail.com>
2023-07-02 14:52:11 -0300

    prevent erros from the user

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ int main() {
 
     int operacao;
     do{
+        do{
         printf("============BEM VINDO AO BANCO LRG============\n\n");
         printf("Operacoes:\n");
         printf("1 -> Criar conta\n");
@@ -24,6 +25,13 @@ int main() {
         printf("0 -> Sair do Banco\n");
 
         scanf("%d", &operacao);
+
+        if(operacao >= 0 && operacao <= 4) break;
+        else printf("Operacao invalida\n");
+        }while (operacao != 1 || operacao != 2 || operacao != 3 || operacao != 4 || operacao != 0);
+        
+
+        if(operacao == 1 || operacao == 2 || operacao == 3 || operacao == 4 || operacao == 0){
         switch (operacao){
             case 1: //creat
                 do {
@@ -33,14 +41,24 @@ int main() {
                     printf("Nome do proprietario: "); scanf(" %[^\n]", account.n_proprietario);
                     printf("Sado inicial da conta: "); scanf("%lf", &account.saldo);
                     scanf("%c", &account.tipo); //esse scanf armazena o \n do scan acima. É uma gambiarra.
+                    
+                    do{ // operacao invalida para char nao funciona
                     printf("Tipo da conta (c/p): "); scanf("%c", &account.tipo);
-
+                    if(account.tipo == 'c' || account.tipo == 'p') break;
+                    else printf("Conta invalida\n");
+                    }while(account.tipo != 'c' || account.tipo != 'p');
+                    
                     vet_contas = createAccount(vet_contas, account, &conta_tam, &conta_id);
                     printf("conta com os dados a seguir acaba de ser criada!: \n");
                     describeAccount(vet_contas[conta_tam - 1]);
-
+                    
+                    do{
                     printf("deseja adicinar nova conta? (1/0): ");
                     scanf("%d", &cont);
+                     if(cont == 1 || cont == 0) break;
+                    else printf("Operacao invalida\n");
+                    }while (cont != 1 || cont != 0);
+                    
                 } while (cont);
                 break;         
                 
@@ -59,8 +77,13 @@ int main() {
                         describeAccount(vet_contas[index_pesquisa]);
                     }
 
-                    printf("deseja pesquisar outra conta? (1/0): ");
-                    scanf("%d", &cont); 
+                    do{
+                    printf("deseja pesquisar uma nova conta? (1/0): ");
+                    scanf("%d", &cont);
+                    if(cont == 1 || cont == 0) break;
+                    else printf("Operacao invalida\n");
+                    }while (cont != 1 || cont != 0);
+
                 }while (cont);
                 break;
                 
@@ -98,9 +121,14 @@ int main() {
                     }else{
                         (printf("ID nao encontrado\n"));
                     }
+                    
+                    do{
+                    printf("deseja sacar ou depositar em uma nova conta? (1/0): ");
+                    scanf("%d", &cont);
+                    if(cont == 1 || cont == 0) break;
+                    else printf("Operacao invalida\n");
+                    }while (cont != 1 || cont != 0);
 
-                    printf("deseja sacar ou depositar novamente nessa ou outra conta? (1/0): ");
-                    scanf("%d", &cont); 
                 }while(cont);
                 break;
 
@@ -119,8 +147,12 @@ int main() {
                         (printf("ID nao encontrado\n"));
                     } 
                     
+                    do{
                     printf("deseja remover outra conta? (1/0): ");
-                    scanf("%d", &cont); 
+                    scanf("%d", &cont);
+                    if(cont == 1 || cont == 0) break;
+                    else printf("Operacao invalida\n");
+                    }while (cont != 1 || cont != 0); 
                 }while (cont);
                 break;
             case 0:
@@ -130,6 +162,10 @@ int main() {
                 printf("Opção inválida\n");
             break;
         }
+        } else {
+            printf("Esta operacaoo é invalida, por favor digite uma nova operacaoo\n\n");
+            continue;
+            }
     }while(operacao);
 
     printf("======== DADOS DE TODAS AS CONTAS APÓS A EXECUÇÃO DAS OPERAÇÕES ========\n");
